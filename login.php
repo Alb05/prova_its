888c101c874c51ece489c4502a1ec1e6dfd68dde
Alb05 <cozzi.alberto05@gmail.com>
2017-12-13 15:44:56 +0100

    completed registation

--- login.php
+++ login.php
@@ -1,45 +1,50 @@
 <?php
 include('conn.php');
+try {
+  // aggiungo gli header per restituire json
+  header('Content-Type: application/json;charset=utf-8');
+  header('Access-Control-Allow-Origin: *');
 
-// aggiungo gli header per restituire json
-header('Content-Type: application/json;charset=utf-8');
-header('Access-Control-Allow-Origin: *');
+  // mi faccio dare dall'utente usrname e password
+  $username = $_GET['username'];
+  $password = $_GET['password'];
 
-// mi faccio dare dall'utente usrname e password
-$username = $_POST['usrn'];
-$password = $_POST['pwd'];
+  // creo la query parametrizzata
+  $query = 'SELECT * FROM USERS WHERE USERNAME = :usrn';
 
-// creo la query parametrizzata
-$query = 'SELECT SALT, PASSWORD FROM USERS WHERE USERNAME = :usrn';
+  // creo un array che conterrà lq righe restituitedal db
+  $data = array();
 
-// creo un array che conterrà lq righe restituitedal db
-$data = array();
+  // eseguo la query sul db passando l'username al parametro :usrn
+  $statement = oci_parse($conn, $query);
+  oci_bind_by_name($statement, ':usrn', $username);
 
-// eseguo la query sul db passando l'username al parametro :usrn
-$statement = oci_parse($conn, $query);
-oci_bind_by_name($statement, ':usrn', $username);
-oci_execute($statement);
+  if (oci_execute($statement)) {
+    // mi faccio dare dal db le righe e le aggiungo all'array
+    while ($row = oci_fetch_assoc($statement)) {
+      $data[] = $row;
+    }
 
-// mi faccio dare dal db le righe e le aggiungo all'array
-while ($row = oci_fetch_assoc($statement)) {
-  $data[] = $row;
-}
-
-// controllo che esista l'utente
-if (count($data) > 0) {
-  $user = $data[0];
-  // creo un hash con la password fornita dall'utente e il sale ottenuto dal db
-  $input = hash('sha512', $password.$user['SALT']);
-  //controllo che la password sia uguale alla password sul db
-  if (strcmp($input, $user['PASSWORD']) == 0) {
-    echo json_encode(true);
+    // controllo che esista l'utente
+    if (count($data) > 0) {
+      $user = $data[0];
+      // creo un hash con la password fornita dall'utente e il sale ottenuto dal db
+      $input = hash('sha512', $password.$user['SALT']);
+      //controllo che la password sia uguale alla password sul db
+      if (strcmp($input, $user['PASSWORD']) == 0) {
+        echo json_encode($data[0]);
+      } else {
+        echo json_encode(NULL);
+      }
+    } else {
+      echo json_encode(NULL);
+    }
   } else {
-    echo json_encode(false);
+    echo json_encode(NULL);
   }
-} else {
-  echo json_encode(false);
+  oci_free_statement($statement);
 }
-
-// chiudo le connessioni
-oci_free_statement($statement);
-oci_close($conn);
+finally {
+  // chiudo le connessioni
+  oci_close($conn);
+}
