bdd4006266d373325813ec9d6893f6d62c395eb9
Alberto Cozzi <cozzi.alberto05@gmail.com>
2017-12-13 08:50:04 +0100

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = 'its_group4';
-$password = 'RattoD1Fogna!';
-
-$conn = oci_connect($username, $password, '(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 212.4.10.98)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = ITS)))');
-if (!$conn) {
-  $e = oci_error();
-  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-}
